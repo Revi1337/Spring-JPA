761d6788fe4492ed40ff055fc8c7fd9fd195900e
Revi1337 <david122123@gmail.com>
2023-02-12 01:34:20 +0900

    단방향 연관관계 설명 수정

--- JPA/OneWayMapping/src/main/java/OneWayMapping/Member.java
+++ JPA/OneWayMapping/src/main/java/OneWayMapping/Member.java
@@ -7,10 +7,12 @@ public class Member {
 
     @Id @GeneratedValue @Column(name = "MEMBER_ID") private Long id;
     @Column(name = "USERNAME") private String username;
-//    @Column(name = "TEAM_ID") private Long teamId;  // TODO 객체지향스럽지 못한 방법. (직접 ID 를 외래키로 들고있음.)
 
     // TODO 1. Member 와 Team 의 관계가 어떤관계인지 JPA 에게 말해주어야함. (하나의 Team 에는 여러명의 Member 가 들어갈 수 있음. 따라서 Member 입장에서 Team 은 N:1 이니까 ManyToOne 으로 매핑해야함.)
-    // TODO 2. 여기서 중요한 것은, JOIN 할 Team 의 컬럼을 선택해주어야 함. (Team 의 PK 를 Member 테이블에 TEAM_ID 컬럼으로 저장하겠다는 것임(FK). 이렇게하면 Member 테이블에 TEAM_ID 컬럼이 생김.)
+    // TODO 2. 여기서 중요한 것은, JOIN 할 컬럼을 선택해주어야 함.
+    // - @JoinColumn 의 name 속성은 그저 `MEMBER 테이블의 컬럼명을 지정` 하는 것뿐 Team 과 매핑하는것과는 아무 관련이 없음. (실제로 MEMBER 테이블에 TEAM_ID 라는 컬럼이 생겨셔 참조할수있게됨.)
+    // - Member 와 Team 을 연관관계(매핑)맺는것은 referencedColumnName 속성이하는데, 이것을 생략하면 자동으로 Team 의 PK 값으로 연관관계를 맺어줘서 생략이 가능하다.
+    //   결과적으로 Team 의 PK 는 MEMBER 테이블의 FK 로 사용되며, 이 FK 컬럼의 이름이 TEAM_ID 인 것임.
     @ManyToOne @JoinColumn(name = "TEAM_ID") private Team team;
 
 
