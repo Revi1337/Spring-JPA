ad37239710d3558f47952e3528291b8411c2e038
Revi1337 <david122123@gmail.com>
2023-03-02 01:53:09 +0900

    3, 공통 인터페이스 설정

--- DataJPA/SpringDataJpa/src/main/java/com/example/springdatajpa/SpringDataJpaApplication.java
+++ DataJPA/SpringDataJpa/src/main/java/com/example/springdatajpa/SpringDataJpaApplication.java
@@ -2,8 +2,10 @@ package com.example.springdatajpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "com.example.springdatajpa.repository") // 스프링을 사용하면 레포지토리위치를 지정해주어야 함. (스프링 부트 사용 시 생략 가능)
 public class SpringDataJpaApplication {
 
     public static void main(String[] args) {
--- DataJPA/SpringDataJpa/src/test/java/com/example/springdatajpa/repository/MemberRepositoryTest.java
+++ DataJPA/SpringDataJpa/src/test/java/com/example/springdatajpa/repository/MemberRepositoryTest.java
@@ -33,4 +33,12 @@ class MemberRepositoryTest {
         assertThat(savedMember).isEqualTo(findMember);
 
     }
+
+    @Test
+    @DisplayName(value = "JpaRepository 의 정체")
+    public void identifyMemberRepository() throws Exception {
+        // TODO 개발자는 JpaRepository 인터페이스만 선언해주면, Spring DataJpa 스스로 JpaRepository 의 구현체를 만들어서 인젝션을 해주는 것임.
+        // Spring DataJpa 가 애플리케이션 로딩시점에 JpaRepository 인터페이스와 관련된 레포지토리(여기서는 MemberRepository)의 구현체를 만들어서 인젝션해주는 것임.
+        System.out.println("JpaRepository Class = " + memberRepository.getClass().getName());
+    }
 }
