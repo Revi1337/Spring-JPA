15920b1016f433904fbb48659379d54183b58434
Revi1337 <david122123@gmail.com>
2023-03-03 00:19:04 +0900

    11. 순수 JPA 로 Paging

--- DataJPA/SpringDataJpa/src/main/java/com/example/springdatajpa/repository/MemberJpaRepository.java
+++ DataJPA/SpringDataJpa/src/main/java/com/example/springdatajpa/repository/MemberJpaRepository.java
@@ -56,4 +56,20 @@ public class MemberJpaRepository {
                 .getResultList();
     }
 
+    // 순수 JPA 로 페이징 구현 1
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m from Member as m where m.age = :age order by m.username desc", Member.class)
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    // 순수 JPA 로 페이징 구현 2
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) from Member as m where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
+
 }
