85549d200e709da315371034103b6b73606cf3ca
Revi1337 <david122123@gmail.com>
2023-03-04 22:04:52 +0900

    19. Web 확장 -  페이징과 정렬

--- DataJPA/SpringDataJpa/src/main/java/com/example/springdatajpa/controller/MemberController.java
+++ DataJPA/SpringDataJpa/src/main/java/com/example/springdatajpa/controller/MemberController.java
@@ -1,11 +1,17 @@
 package com.example.springdatajpa.controller;
 
+import com.example.springdatajpa.dto.MemberDto;
 import com.example.springdatajpa.entity.Member;
 import com.example.springdatajpa.repository.MemberRepository;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController @RequiredArgsConstructor
@@ -15,7 +21,8 @@ public class MemberController {
 
     @PostConstruct
     public void init() {
-        memberRepository.save(new Member("userA"));
+        for (int i = 0; i < 100; i++)
+            memberRepository.save(new Member("user" + i, i));
     }
 
     @GetMapping(value = "/members/{id}")
@@ -27,6 +34,21 @@ public class MemberController {
     @GetMapping(value = "/members2/{id}")
     public String findMember2(@PathVariable(name = "id") Member member) {
         return member.getUsername();
+    }
+
+    @GetMapping(value = "/members")
+    public Page<MemberDto> list(Pageable pageable) {
+//        Page<Member> page = memberRepository.findAll(pageable);
+//        Page<MemberDto> memberDto = page.map(member -> new MemberDto(member.getId(), member.getUsername(), null));
+//        return memberDto;
+        return memberRepository.findAll(pageable)
+                .map(MemberDto::new);
+    }
+
+    @GetMapping(value = "/members2")
+    public Page<Member> list2(@PageableDefault(size = 4, sort = "username") Pageable pageable) {
+        Page<Member> page = memberRepository.findAll(pageable);
+        return page;
     }
 
 }
