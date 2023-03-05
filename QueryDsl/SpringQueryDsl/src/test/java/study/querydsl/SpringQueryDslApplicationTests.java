0dd17842d71ad1b8638a7647f56eae6e9fff5716
Revi1337 <david122123@gmail.com>
2023-03-05 23:30:16 +0900

    2. QueryDsl - JPA 설정

--- QueryDsl/SpringQueryDsl/src/test/java/study/querydsl/SpringQueryDslApplicationTests.java
+++ QueryDsl/SpringQueryDsl/src/test/java/study/querydsl/SpringQueryDslApplicationTests.java
@@ -5,13 +5,15 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest @Transactional
+@SpringBootTest @Transactional @Commit // Rollback(value = false) 를 대신해서 쓸 수 있음.
 class SpringQueryDslApplicationTests {
 
     @PersistenceContext EntityManager em;
