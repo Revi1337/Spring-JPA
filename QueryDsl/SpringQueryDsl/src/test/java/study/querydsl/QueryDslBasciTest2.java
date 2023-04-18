316dca0f95b9120db4e4965de631a348552fa3b9
Revi1337 <david122123@gmail.com>
2023-04-19 00:23:27 +0900

    16. QueryDsl - CASE
    음....
    말그대로 음...
    과연 이걸 많이 사용할까라는 생각이 든다

--- QueryDsl/SpringQueryDsl/src/test/java/study/querydsl/QueryDslBasciTest2.java
+++ QueryDsl/SpringQueryDsl/src/test/java/study/querydsl/QueryDslBasciTest2.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.Tuple;
+import com.querydsl.core.types.dsl.CaseBuilder;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.*;
@@ -425,5 +426,33 @@ public class QueryDslBasciTest2 {
     }
 
 
+    @Test
+    @DisplayName(value = "CASE 문 사용 (Simple Case)")
+    public void basicCase1() {
+        List<String> result = query
+                .select(member.age
+                        .when(10).then("열살")
+                        .when(20).then("스무살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 
+    @Test
+    @DisplayName(value = "CASE 문 사용 (Complex Case) --> CaseBuilder 를 사용")
+    public void basicCase2() {
+        List<String> result = query
+                .select(new CaseBuilder()
+                        .when(member.age.between(0, 20)).then("0~20살")
+                        .when(member.age.between(21, 30)).then("21~30살")
+                        .otherwise("기타"))
+                .from(member)
+                .fetch();
+        for (String s : result) {
+            System.out.println("s = " + s);
+        }
+    }
 }
