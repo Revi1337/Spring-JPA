56fca722e49237998c9c6483500f66aa31e08abf
Revi1337 <david122123@gmail.com>
2023-04-24 17:44:29 +0900

    26-1. QueryDsl - DataJPA 의 페이징 활용2 (최적화)

--- QueryDsl/SpringQueryDsl/DataJpaWithQueryDsl/src/main/java/com/example/datajpawithquerydsl/repository/MemberRepositoryCustomImpl.java
+++ QueryDsl/SpringQueryDsl/DataJpaWithQueryDsl/src/main/java/com/example/datajpawithquerydsl/repository/MemberRepositoryCustomImpl.java
@@ -3,13 +3,16 @@ package com.example.datajpawithquerydsl.repository;
 import com.example.datajpawithquerydsl.dto.MemberSearchCondition;
 import com.example.datajpawithquerydsl.dto.MemberTeamDto;
 import com.example.datajpawithquerydsl.dto.QMemberTeamDto;
+import com.example.datajpawithquerydsl.entity.Member;
 import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
@@ -93,17 +96,34 @@ public class MemberRepositoryCustomImpl implements MemberRepositoryCustom {
                 .fetch();
 
         // count 쿼리르 따로 분리
-        long totalCount = queryFactory
+//        long totalCount = queryFactory
+//                .selectFrom(member)
+//                .leftJoin(member.team, team)
+//                .where(
+//                        usernameEq(condition.getUsername()),
+//                        teamNameEq(condition.getTeamName()),
+//                        ageGoe(condition.getAgeGoe()),
+//                        ageLoe(condition.getAgeLoe()))
+//                .fetchCount();
+//
+//        return new PageImpl<>(content, pageable, totalCount);
+
+
+        // count 쿼리가 생략 가능한 경우는 아래 두가지
+        // 1. 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작을 때
+        // 2. 마지막 페이지 일 때 (offset + 컨텐츠 사이즈를 더해서 전체 사이즈 구함)
+        JPAQuery<Member> countQuery = queryFactory                             // .fetchCount() 로 카운트 쿼리를 일단 보류
                 .selectFrom(member)
                 .leftJoin(member.team, team)
                 .where(
                         usernameEq(condition.getUsername()),
                         teamNameEq(condition.getTeamName()),
                         ageGoe(condition.getAgeGoe()),
-                        ageLoe(condition.getAgeLoe()))
-                .fetchCount();
+                        ageLoe(condition.getAgeLoe())
+                );
 
-        return new PageImpl<>(content, pageable, totalCount);
+        // 페이지 시작이면서 컨텐츠 사이즈가 페이지 사이즈보다 작거나, 마지막 페이지일때만 countQuery 를 호출해줌.
+        return PageableExecutionUtils.getPage(content, pageable, () -> countQuery.fetchCount());
     }
 
     private BooleanExpression usernameEq(String username) {
--- QueryDsl/SpringQueryDsl/DataJpaWithQueryDsl/src/test/java/com/example/datajpawithquerydsl/repository/MemberRepositoryTest.java
+++ QueryDsl/SpringQueryDsl/DataJpaWithQueryDsl/src/test/java/com/example/datajpawithquerydsl/repository/MemberRepositoryTest.java
@@ -89,7 +89,8 @@ class MemberRepositoryTest {
 
         MemberSearchCondition memberSearchCondition = new MemberSearchCondition();
         PageRequest pageRequest = PageRequest.of(0, 3);
-        Page<MemberTeamDto> result = memberRepository.searchPageSimple(memberSearchCondition, pageRequest);
+//        Page<MemberTeamDto> result = memberRepository.searchPageSimple(memberSearchCondition, pageRequest);
+        Page<MemberTeamDto> result = memberRepository.searchPageComplex(memberSearchCondition, pageRequest);
 
         assertThat(result.getSize()).isEqualTo(3);
         assertThat(result.getContent()).extracting("username").containsExactly("member1", "member2", "member3");
