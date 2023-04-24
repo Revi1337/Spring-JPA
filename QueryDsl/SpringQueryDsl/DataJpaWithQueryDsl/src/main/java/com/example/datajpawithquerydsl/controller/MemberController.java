aa57354d199084e1393a9780a8eaf19ba3d1a00e
Revi1337 <david122123@gmail.com>
2023-04-24 18:01:25 +0900

    26-2. QueryDsl - DataJPA 의 페이징 활용 - API 개발

--- QueryDsl/SpringQueryDsl/DataJpaWithQueryDsl/src/main/java/com/example/datajpawithquerydsl/controller/MemberController.java
+++ QueryDsl/SpringQueryDsl/DataJpaWithQueryDsl/src/main/java/com/example/datajpawithquerydsl/controller/MemberController.java
@@ -3,9 +3,13 @@ package com.example.datajpawithquerydsl.controller;
 
 import com.example.datajpawithquerydsl.dto.MemberSearchCondition;
 import com.example.datajpawithquerydsl.repository.MemberQueryDslRepository;
+import com.example.datajpawithquerydsl.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController @RequiredArgsConstructor
@@ -13,11 +17,27 @@ public class MemberController {
 
     private final MemberQueryDslRepository memberQueryDslRepository;
 
+    private final MemberRepository memberRepository;
+
     // http://localhost:8080/v1/members?teamName=teamB&ageGoe=31&ageLoe=35
     @GetMapping(path = "/v1/members")
     public ResponseEntity<?> searchMemberV1(MemberSearchCondition memberSearchCondition) {
         return ResponseEntity.ok(
                 memberQueryDslRepository.search(memberSearchCondition)
+        );
+    }
+
+    @GetMapping(path = "/v2/members")
+    public ResponseEntity<?> searchMemberV2(MemberSearchCondition memberSearchCondition, Pageable pageable) {
+        return ResponseEntity.ok(
+                memberRepository.searchPageSimple(memberSearchCondition, pageable)
+        );
+    }
+
+    @GetMapping(path = "/v3/members")
+    public ResponseEntity<?> searchMemberV3(MemberSearchCondition memberSearchCondition, Pageable pageable) {
+        return ResponseEntity.ok(
+                memberRepository.searchPageComplex(memberSearchCondition, pageable)
         );
     }
 
